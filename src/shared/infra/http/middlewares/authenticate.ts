4d6bcac15be11095be4ca8e258b79f8d23b43015
Eduardo Gutkoski <eduardo.pedogutkoski@gmail.com>
2022-11-18 21:21:53 -0300

    feat: auth middleware

---
+++ src/shared/infra/http/middlewares/authenticate.ts
@@ -0,0 +1,28 @@
+import auth from '@config/auth';
+import { AppError } from '@shared/errors/AppError';
+import { NextFunction, Request } from 'express';
+import { verify } from 'jsonwebtoken';
+
+interface IPayload {
+  sub: string;
+}
+
+export async function authenticate(request: Request, next: NextFunction) {
+  const authHeader = request.headers.authorization;
+
+  if (!authHeader) throw new AppError('missing-token', 401);
+
+  const [, token] = authHeader.split(' ');
+
+  try {
+    const { sub: userId } = verify(token, auth.secret_token) as IPayload;
+
+    request.user = {
+      id: userId,
+    };
+
+    next();
+  } catch (error) {
+    throw new AppError('invalid-token', 401);
+  }
+}
